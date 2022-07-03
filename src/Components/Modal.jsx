02ee592a66fb5c8212b1fdd98d6bfdd4df65d3a7
ayushdotdev <ayush.11914366@lpu.in>
2022-07-03 16:53:49 +0530

    second commit

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,40 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const Modal = (props) => {
+export default function Navbar() {
   return (
-    <div className='static'>
-        <div className="fixed h-screen w-screen bg-black z-10 top-0 opacity-75"></div>
-
-         <div className='fixed  top-0 right-0 z-20 flex justify-center'>
-            <div className='mx-4 my-4 bg-white'>
-                <div className='flex justify-end'>
-                    <button className='border-2 text-red-900 px-2 m-2'
-                    >X</button>
-                </div>
-                <div className='bg-white'>
-                <img className="w-full" src="https://cdn.shopify.com/s/files/1/1626/8507/products/classic-dad-hat-pink-front-620a928e93e58_345x550.jpg?v=1644860054"
-                 alt="Sunset in the mountains"
-                 />
-                 <div className='flex justify-between px-6 py-1'>
-<div className='font-bold text-xl'>The Coldest Sunset</div>
-<div className="font-bold font-mono text-xl text-red-700">$35</div>
-                 </div>
-                 <div className='flex justify-around items-center px-2 py-1'>
-                 <button className="border-2 px-2">-</button>
-                 <div className="font-bold font-mono text-xl text-red-700">Quanity: 1</div>
-                 <button className="border-2 px-2">+</button>
-                 </div>
-                 <div className='flex justify-around items-center px-2 py-1'>
-                    <button className=
-                    'border-2 px-2 py-1 rounded bg-green-500 text-white font-bold font-mono text-lg'
-                    >Add to Cart</button>
-                 </div>
-            </div>
-            </div>
-            
-         </div>
-    </div>
-  )
+    <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-4">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <svg
+          className="fill-current h-8 w-8 mr-2"
+          width="54"
+          height="54"
+          viewBox="0 0 54 54"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+        </svg>
+        <span className="font-semibold text-xl tracking-tight">
+          Tailwind Shop
+        </span>
+      </div>
+      <div className="block">
+        {/** lg:hidden */}
+        <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg
+            className="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </button>
+      </div>
+    </nav>
+  );
 }
-
-export default Modal
